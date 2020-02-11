663da569e426f0ca0165619269148fb603dbe4f8
denisrybalka <60781576+denisrybalka@users.noreply.github.com>
2020-02-11 17:08:33 +0200

    Add files via upload

---
+++ irvas/js/main.js
@@ -0,0 +1,20 @@
+$('.order-btn').click(function() {
+	$('main').css('filter','blur(5px)');
+	$('.modal').fadeIn();
+});
+
+$('.close-popup').click(function() {
+	$('.modal').fadeOut();
+	$('main').css('filter','none');
+});
+
+$('.recall-btn').click(function() {
+	$('.modal').fadeOut();
+	$('.thanks-modal').fadeIn()
+});
+
+$('.thanks-close-popup').click(function() {
+	$('.thanks-popup').fadeOut();
+	$('main').css('filter','none');
+});
+
